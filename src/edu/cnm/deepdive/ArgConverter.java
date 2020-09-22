9bc6b8ad7d3f8ab59e112c4428424769d2e9bbfe
Adrian Anaya <aanaya79@cnm.edu>
2020-09-22 16:18:52 -0600

    Catching up with commits

--- src/edu/cnm/deepdive/ArgConverter.java
+++ src/edu/cnm/deepdive/ArgConverter.java
@@ -3,7 +3,7 @@ package edu.cnm.deepdive;
 public class ArgConverter {
 
   public static void main(String[] args) {
-    double celsius = Double.parseDouble(args[0]);
+    double celsius = Double.parseDouble(args[ 0]);
     double fahrenheit = InputConverter.celsiusToFahrenheit(celsius);
     System.out.println(fahrenheit);
   }
